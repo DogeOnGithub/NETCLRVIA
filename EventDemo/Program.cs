6e24d12558de9089913c1dd3ab6757314de19d66
TjSanshao <1305705188@qq.com>
2018-03-22 23:40:00 +0800

    事件模式Demo

---
+++ EventDemo/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading;
+
+namespace EventDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MailManager manager = new MailManager();
+            MailClient client = new MailClient();
+            MailClient client2 = new MailClient();
+            manager.NewMail += client.OnNewMail;
+            manager.NewMail += client2.OnNewMail;
+            manager.OnNewMail(new NewMailEventArgs("tj", "tj2", "test"));
+            Console.ReadKey();
+        }
+    }
+
+    class NewMailEventArgs : EventArgs
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+
+        public NewMailEventArgs(string from, string to, string subject)
+        {
+            From = from;
+            To = to;
+            Subject = subject;
+        }
+    }
+
+    class MailManager
+    {
+        public event EventHandler<NewMailEventArgs> NewMail;
+
+        public void OnNewMail(NewMailEventArgs e)
+        {
+            EventHandler<NewMailEventArgs> temp = Volatile.Read(ref NewMail);
+            if (temp != null)
+            {
+                temp(this, e);
+            }
+        }
+    }
+
+    class MailClient
+    {
+        public void OnNewMail(object obj, NewMailEventArgs args)
+        {
+            Console.WriteLine(args.From);
+            Console.WriteLine(args.To);
+            Console.WriteLine(args.Subject);
+        }
+    }
+}
