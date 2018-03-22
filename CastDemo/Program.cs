831a5e52668308452059f11c7c2822e4f3f7c052
TjSanshao <1305705188@qq.com>
2018-03-22 12:04:07 +0800

    类型转换操作符Demo

---
+++ CastDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person(22);
+            Console.WriteLine(person.Name + person.Age);
+            Console.WriteLine(person.ToInt32());
+
+            Console.WriteLine("___________________________");
+
+            Person person2 = 11;
+            Person person3 = (Person)"TjSanshao";
+
+            Console.WriteLine(person2.Name + person2.Age);
+
+            Console.WriteLine("___________________________");
+
+            Console.WriteLine(person3.Name + person3.Age);
+
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public Person(int age)
+        {
+            this.Age = age;
+            this.Name = "TjSanshao";
+        }
+        
+        public int ToInt32()
+        {
+            return this.Age;
+        }
+
+        public static implicit operator Person(int age)
+        {
+            return new Person(age);
+        }
+
+        public static explicit operator Person(string name)
+        {
+            return new Person(22);
+        }
+    }
+}
