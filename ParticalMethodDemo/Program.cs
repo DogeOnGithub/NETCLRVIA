6b38bfc68e224595b046bb9afc9d191e321b5e2c
TjSanshao <1305705188@qq.com>
2018-03-22 13:11:35 +0800

    分部方法Demo

---
+++ ParticalMethodDemo/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticalMethodDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person { Name = "tj" };
+            Console.WriteLine(person.Name);
+
+            person.Name = "tj2";
+            Console.WriteLine(person.Name);
+
+            Console.ReadKey();
+        }
+    }
+
+    /// <summary>
+    /// 假设这里是用工具生成的代码
+    /// </summary>
+    partial class Person
+    {
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { OnNameChanging(value.ToUpper()); name = value; }
+        }
+
+        partial void OnNameChanging(string value);
+    }
+
+    /// <summary>
+    /// 假设这里是自己写的代码
+    /// </summary>
+    partial class Person
+    {
+        partial void OnNameChanging(string value)
+        {
+            Console.WriteLine("jjjjjj");
+        }
+    }
+}
