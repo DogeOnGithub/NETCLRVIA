f0bf1c0b3e0b0f8368104b5d767b93a0b66d7a77
TjSanshao <1305705188@qq.com>
2018-03-22 19:56:05 +0800

    Tuple的小测试

---
+++ TupleDemo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TupleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tuple<int, int> tuple = Min(10, 20);
+            Console.WriteLine(tuple.Item1);
+            Console.WriteLine(tuple.Item2);
+            Console.ReadKey();
+        }
+
+        static Tuple<int, int> Min(int a, int b)
+        {
+            return new Tuple<int, int>(Math.Min(a, b), Math.Max(a, b));
+        }
+    }
+}
