d27713ef8b4608c310e15c0dc9500ad61a256d91
TjSanshao <1305705188@qq.com>
2018-03-23 19:37:36 +0800

    关于接口的显式实现和隐式实现

---
+++ InterfaceDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IPerson person = new Person();
+            person.Print();
+            Console.ReadKey();
+
+            Person person2 = new Person();
+            person2.Print();
+            Console.ReadKey();
+        }
+    }
+
+    interface IPerson
+    {
+        void Print();
+    }
+
+    class Person : IPerson
+    {
+        void IPerson.Print()
+        {
+            Console.WriteLine("Private IPerson");
+        }
+
+        public void Print()
+        {
+            Console.WriteLine("Public Person");
+        }
+    }
+}
