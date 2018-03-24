1fd9194a1a99200f2aec5f83cdc440fd77537179
TjSanshao <1305705188@qq.com>
2018-03-25 02:07:45 +0800

    位标志Demo

---
+++ FlagsDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlagsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Actions action = Actions.RW;
+            Console.WriteLine(action.ToString());
+            action = Actions.Read | Actions.Delete;
+            Console.WriteLine(action);
+            Console.ReadKey();
+        }
+
+        [Flags]
+        enum Actions
+        {
+            None = 0,
+            Read = 0x0001,
+            Write = 0x0002,
+            RW = Actions.Read | Actions.Write,
+            Delete = 0x0004
+        }
+    }
+}
