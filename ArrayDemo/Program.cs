28f140240c2037f6726a5e96454d5cb193951ec6
TjSanshao <1305705188@qq.com>
2018-03-25 17:55:01 +0800

    添加注释

--- ArrayDemo/Program.cs
+++ ArrayDemo/Program.cs
@@ -8,8 +8,12 @@ namespace ArrayDemo
         {
             int[] lowerBounds = { 2005, 1 };
             int[] lengths = { 5, 4 };
+
+            //利用Array类型的静态方法，可以创建下标起始不为0的数组
             decimal[,] quarterlyRevenue = (decimal[,])Array.CreateInstance(typeof(decimal), lengths, lowerBounds);
+
             Console.WriteLine("{0,4}{1,9}{2,9}{3,9}{4,9}", "Year", "Q1", "Q2", "Q3", "Q4");
+
             int firstYear = quarterlyRevenue.GetLowerBound(0);
             int lastYear = quarterlyRevenue.GetUpperBound(0);
             int firstQ = quarterlyRevenue.GetLowerBound(1);
