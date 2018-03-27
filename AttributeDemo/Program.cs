a84261c7ba4d8376c48ee27586a15575a9f31a9b
TjSanshao <1305705188@qq.com>
2018-03-27 13:42:47 +0800

    显式调用委托链

---
+++ AttributeDemo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+
+    [AttributeUsage(AttributeTargets.All)]
+    class SomeAttribute : Attribute
+    {
+        public SomeAttribute(string name, object obj, Type[] types)
+        {
+
+        }
+    }
+
+    enum Color
+    {
+        Red,
+        Black
+    }
+
+    [Some("TjSanshao", Color.Red, new Type[] { typeof(string), typeof(Color) })]
+    class Test
+    {
+
+    }
+}
