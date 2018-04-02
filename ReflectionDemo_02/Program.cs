8931beafd5f79b32f38d51f026c84f39683ae02c
TjSanshao <1305705188@qq.com>
2018-04-02 22:23:38 +0800

    构造泛型类型

---
+++ ReflectionDemo_02/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ReflectionDemo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Type openType = typeof(Dictionary<,>);
+
+            Type closeType = openType.MakeGenericType(typeof(string), typeof(int));
+
+            object obj = Activator.CreateInstance(closeType);
+
+            Console.WriteLine(obj.GetType());
+
+            Console.ReadKey();
+        }
+    }
+}
