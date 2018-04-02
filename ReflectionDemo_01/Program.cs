24aa471083ef97d215c4f869cc27985533c87cec
TjSanshao <1305705188@qq.com>
2018-04-02 09:09:07 +0800

    简单的反射应用Demo

---
+++ ReflectionDemo_01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+
+namespace ReflectionDemo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly exeAssembly = Assembly.GetExecutingAssembly();
+            foreach (var c in exeAssembly.DefinedTypes)
+            {
+                Console.WriteLine(c.FullName);
+                Console.WriteLine(c.DeclaredMethods.FirstOrDefault());
+                Console.WriteLine("--------------------------------------");
+            }
+            Console.ReadKey();
+        }
+    }
+
+    public class Person
+    {
+
+    }
+}
