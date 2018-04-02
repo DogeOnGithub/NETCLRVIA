baabcd6e85a54876962361cef88d8328160102d4
TjSanshao <1305705188@qq.com>
2018-04-02 23:11:13 +0800

    使用反射发现类型成员

---
+++ ReflectionDemo_03/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Reflection;
+
+namespace ReflectionDemo_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            foreach (var assembly in assemblies)
+            {
+                Console.WriteLine("Assembly:" + assembly.FullName);
+                foreach (var type in assembly.DefinedTypes)
+                {
+                    Console.WriteLine("Type:" + type.FullName);
+                    foreach (var member in type.GetMembers())
+                    {
+                        Console.WriteLine("Member:" + member.Name);
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
