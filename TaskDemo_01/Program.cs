0159e2c64ac95931b5c03b583c7b67a204d4c091
TjSanshao <1305705188@qq.com>
2018-04-10 20:39:02 +0800

    启动任务

---
+++ TaskDemo_01/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskDemo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ThreadPool.QueueUserWorkItem(Print, "TjSanshao");
+
+            Task task = new Task(Print, "TjSanshao");
+            task.Start();
+
+            Task.Run(() => Print("TjSanshao"));
+
+            //该类继承自Task，泛型参数指的是返回类型
+            Task<int> t = new Task<int>((n) => Count((int)n), 1000);
+            t.Start();
+            //调用Result时，内部会使用Task.Wait()方法
+            Console.WriteLine(t.Result);
+
+            Console.ReadKey();
+        }
+
+        static void Print(object name)
+        {
+            Console.WriteLine("Name : " + name);
+        }
+
+        static int Count(int n)
+        {
+            Thread.Sleep(3000);
+            return n + 1000;
+        }
+    }
+}
