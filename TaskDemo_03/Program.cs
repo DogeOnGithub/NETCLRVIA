1ab781eb48035ea1ddf3af7405c9226a2abbffca
TjSanshao <1305705188@qq.com>
2018-04-11 00:36:49 +0800

    任务完成时启动新任务

---
+++ TaskDemo_03/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskDemo_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task task1 = Task.Run(() => Console.WriteLine("TjSanshao"));
+            Task task2 = task1.ContinueWith((t) => Console.WriteLine("Tj2" + t.Id));
+            Task.WaitAll();
+            Console.ReadKey();
+        }
+    }
+}
