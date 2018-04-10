3d9f660ef8d28281e1042314a25cd5799f1f2508
TjSanshao <1305705188@qq.com>
2018-04-10 16:56:59 +0800

    取消线程操作

---
+++ ThreadDemo_04/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+namespace ThreadDemo_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CancellationDemo.Go();
+        }
+    }
+
+    class CancellationDemo
+    {
+        public static void Go()
+        {
+            CancellationTokenSource cts = new CancellationTokenSource();
+
+            cts.Token.Register(() => Console.WriteLine("TjSanshao"));
+
+            ThreadPool.QueueUserWorkItem((o) => Count(cts.Token, 1000));
+
+            Console.ReadKey();
+
+            cts.Cancel();
+
+            Console.ReadKey();
+        }
+
+        private static void Count(CancellationToken token, int countTo)
+        {
+            for (int i = 0; i < countTo; i++)
+            {
+                if (token.IsCancellationRequested)
+                {
+                    Console.WriteLine("Count is canceled");
+                    break;
+                }
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Count is done");
+        }
+    }
+}
