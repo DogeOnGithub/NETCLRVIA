f0af56878f386c90d7b03e907143c7fe585db06a
TjSanshao <1305705188@qq.com>
2018-04-11 21:33:40 +0800

    Parallel的静态For、ForEach方法

---
+++ ParallelDemo_01/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelDemo_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parallel.For(0, 1000, i => DoWork(i));
+
+            int[] ints = { 1, 2, 3, 4, 5 };
+            Parallel.ForEach(ints, i => DoWork(i));
+
+            Parallel.Invoke(Method1, Method2, Method3);
+
+            Console.ReadKey();
+        }
+
+        static void DoWork(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine(i);
+            }
+        }
+
+        static void Method1()
+        {
+            Console.WriteLine("Method1");
+        }
+
+        static void Method2()
+        {
+            Console.WriteLine("Method2");
+        }
+
+        static void Method3()
+        {
+            Console.WriteLine("Method3");
+        }
+    }
+}
---
+++ ParallelDemo_02/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ParallelDemo_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+
+            stopwatch.Start();
+            long b = DirectoryBytes(@"E:\MySpeciality", "*.*", SearchOption.AllDirectories);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.Milliseconds);
+            Console.WriteLine(b);
+
+            Console.ReadKey();
+
+            stopwatch.Restart();
+            long b2 = DirectoryBytesSync(@"E:\MySpeciality", "*.*", SearchOption.AllDirectories);
+            stopwatch.Stop();
+            Console.WriteLine(stopwatch.Elapsed.Milliseconds);
+            Console.WriteLine(b2);
+
+            Console.ReadKey();
+        }
+
+        static long DirectoryBytes(string path, string searchPattern, SearchOption searchOption)
+        {
+            var files = Directory.EnumerateFiles(path, searchPattern, searchOption);
+
+            long masterTotal = 0;
+
+            //该方法接受3个委托，分别是初始化委托，主体委托，主体委托执行后的终结委托
+            var result = Parallel.ForEach<string, long>(files,
+                //初始化委托
+                () => { return 0; },
+                //主体委托，其中taskLocalTotal是初始化委托中的返回值
+                (file, loopState, index, taskLocalTotal) =>
+                {
+                    //Console.WriteLine(file);
+                    long fileLength = 0;
+                    FileStream fileStream = null;
+                    try
+                    {
+                        fileStream = File.OpenRead(file);
+                        fileLength = fileStream.Length;
+                    }
+                    catch (IOException)
+                    {
+                        //忽略所有无法访问的文件
+                        //throw;
+                    }
+                    finally
+                    {
+                        if (fileStream != null)
+                        {
+                            fileStream.Dispose();
+                        }
+                    }
+                    return taskLocalTotal + fileLength;
+                },
+                //终结委托，其中taskLocalTotal是主体委托执行后的返回值
+                taskLocalTotal => { Interlocked.Add(ref masterTotal, taskLocalTotal); }
+                );
+
+            return masterTotal;
+        }
+
+        static long DirectoryBytesSync(string path, string searchPattern, SearchOption searchOption)
+        {
+            var files = Directory.EnumerateFiles(path, searchPattern, searchOption);
+
+            long masterTotal = 0;
+
+            foreach (var file in files)
+            {
+                FileStream fileStream = null;
+                try
+                {
+                    fileStream = File.OpenRead(file);
+                    masterTotal += fileStream.Length;
+                }
+                catch (IOException)
+                {
+
+                }
+                finally
+                {
+                    if (fileStream != null)
+                    {
+                        fileStream.Dispose();
+                    }
+                }
+            }
+
+            return masterTotal;
+        }
+    }
+}
